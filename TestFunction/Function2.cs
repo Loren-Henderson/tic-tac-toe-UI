cc69d0331864a31725d829e160079f238ba054ee
2000368 <loran.henderson@cognizant.com>
2021-03-01 13:35:29 -0500

    commented out Function2

--- TestFunction/Function2.cs
+++ TestFunction/Function2.cs
@@ -1,16 +1,16 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+// using System;
+// using Microsoft.Azure.WebJobs;
+// using Microsoft.Azure.WebJobs.Host;
+// using Microsoft.Extensions.Logging;
 
-namespace TestFunction
-{
-    public static class Function2
-    {
-        [FunctionName("Function2")]
-        public static void Run([QueueTrigger("QueueName", Connection = "ConnectionStrings:AzureWebJobsStorage")]string myQueueItem, ILogger log)
-        {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-        }
-    }
-}
+// namespace TestFunction
+// {
+//     public static class Function2
+//     {
+//         [FunctionName("Function2")]
+//         public static void Run([QueueTrigger("QueueName", Connection = "ConnectionStrings:AzureWebJobsStorage")]string myQueueItem, ILogger log)
+//         {
+//             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+//         }
+//     }
+// }
